13237fdf510e58219795e02a6af4ec281e62f02f
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-11-23 14:37:24 -0300

    ap10(java): acesso a base de dados

---
+++ Musica.java
@@ -0,0 +1,13 @@
+import lombok.Getter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Musica {
+    private int codigo;
+    private String titulo;
+    private int avaliacao;
+    //ORM Object Relational Mapping
+}
---
+++ MusicaDAO.java
@@ -0,0 +1,19 @@
+import java.sql.PreparedStatement;
+
+public class MusicaDAO {
+    public void cadastrar(Musica musica) throws Exception{
+        //1. Especificar o Comando SQL
+        String sql = "INSERT INTO tb_musica(titulo) VALUES(?)";
+        //2. Estabelecer uma conexão com SGDB (PostgreSQL)
+        var conexao = ConnectionFactory.conectar();
+        //3. Preparar o comando
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        //4. Substituir os eventuais placeholders
+        ps.setString(1, musica.getTitulo());
+        //5. Executar o comando
+        ps.execute();
+        //6. Fechar os recursos
+        ps.close();
+        conexao.close();
+    }
+}
